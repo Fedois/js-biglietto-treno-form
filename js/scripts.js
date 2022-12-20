ec06ff2ca42935f9673fcc162a7fc4ddc82be6a6
Fedois <fedefox14@gmail.com>
2022-12-20 17:30:50 +0100

    sistemazione codice html e css

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,4 @@
 
-
-// let kilometri = parseInt(prompt('quanti kilometri vuoi percorrere?'));
-// console.log('km: ', kilometri)
-
-// const età = parseInt(prompt('quanti anni hai?'));
-// console.log('età: ', età);
-
-// let prezzokm = (kilometri * 0.21).toFixed(2);
-// console.log('prezzo ticket: ', prezzokm + '€');
-
-// if(età < 18){
-//     alert('avrai uno sconto del 20%');
-    
-//     let calcUno = ((prezzokm / 100) * 20).toFixed(2);
-//     console.log('sconto 20%: ', calcUno + '€');
-
-//     let totaleUno = (prezzokm - calcUno).toFixed(2);
-//     console.log('prezzo finale: ', totaleUno + '€')
-    
-//     document.getElementById('prezzo').innerHTML = totaleUno + '€';
-
-// }
-// else if(età >= 65){
-//     alert ('avrai uno sconto del 40%');
-    
-//     let calcDue = ((prezzokm / 100) * 40).toFixed(2);
-//     console.log('sconto 40%: ', calcDue + '€'); 
-
-//     let totaleDue = (prezzokm - calcDue).toFixed(2);
-//     console.log('prezzo finale: ', totaleDue + '€')
-
-//     document.getElementById('prezzo').innerHTML = totaleDue + '€'; 
-// }
-// else{
-//     document.getElementById('prezzo').innerHTML = prezzokm + '€';
-// }
-
 const generaBtn = document.getElementById('genera');
 
 generaBtn.addEventListener('click',
@@ -45,7 +8,7 @@ generaBtn.addEventListener('click',
         document.getElementById('box-bottom').style.display = 'block';
 
         const nomeInput = document.getElementById('nome');
-        console.log('nome cliente: ', nomeInput.value,)
+        console.log('nome cliente: ', nomeInput.value)
         document.getElementById('nome-cliente').innerHTML = nomeInput.value;
 
         const kmInput = document.getElementById('km');
@@ -60,7 +23,7 @@ generaBtn.addEventListener('click',
         if(etaInput.value == 'minnorenne'){
             alert('avrai uno sconto del 20%');
 
-            document.getElementById('ticket').innerHTML = 'Biglietto 1';
+            document.getElementById('ticket').innerHTML = 'Biglietto Baby';
 
             const postoRandom = Math.random();
             document.getElementById('posto').innerHTML = Math.floor(postoRandom * 100);
@@ -81,7 +44,7 @@ generaBtn.addEventListener('click',
         else if(etaInput.value == 'over 65'){
             alert('avrai uno sconto del 40%')
 
-            document.getElementById('ticket').innerHTML = 'Biglietto 2';
+            document.getElementById('ticket').innerHTML = 'Biglietto Silver';
 
             const postoRandom = Math.random();
             document.getElementById('posto').innerHTML = Math.floor(postoRandom * 100);
